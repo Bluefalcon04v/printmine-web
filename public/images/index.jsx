2ddf6e9709c423672daddce853dc3412909f26ce
Vidushi Malik <vidushimalik135@gmail.com>
2025-02-23 18:57:25 +0530

    changed images

--- public/images/index.jsx
+++ public/images/index.jsx
@@ -12,9 +12,19 @@ const SampleImage = "/images/sampleImage.png";
 const ReviewKeyChain = "/images/reviewKeyChain.png";
 const Founder = "/images/founder.png";
 const TeamImage = "/images/teamImage.png";
-const BlogImage = "/images/blogImage.png"
+const BlogImage = "/images/blogImage.png";
+const WatchBuy1 = "/images/watchBuy1.png";
+const WatchBuy2 = "/images/watchBuy2.png";
+const WatchBuy3 = "/images/watchBuy3.png";
+const WatchBuy4 = "/images/watchBuy4.png";
+const WatchBuy5 = "/images/watchBuy5.png";
 
 export {
+  WatchBuy5,
+  WatchBuy4,
+  WatchBuy3,
+  WatchBuy2,
+  WatchBuy1,
   BlogImage,
   TeamImage,
   Founder,
--- src/components/homePage/watchAndBuy.tsx
+++ src/components/homePage/watchAndBuy.tsx
@@ -1,35 +1,35 @@
-import { PenImage } from "@public/images";
+import { PenImage, WatchBuy1, WatchBuy2, WatchBuy3, WatchBuy4, WatchBuy5 } from "@public/images";
 import Image from "next/image";
 import React from "react";
 
 const watchAndBuyData = [
   {
-    image: PenImage,
+    image: WatchBuy2,
     name: "Premium Pen",
     sellingPrice: 200,
     actualPrice: 400,
   },
   {
-    image: PenImage,
-    name: "Premium Pen",
+    image: WatchBuy1,
+    name: "Perfume Bottle",
+    sellingPrice: 500,
+    actualPrice: 800,
+  },
+  {
+    image: WatchBuy3,
+    name: "Steel Key",
     sellingPrice: 200,
     actualPrice: 400,
   },
   {
-    image: PenImage,
-    name: "Premium Pen",
+    image: WatchBuy4,
+    name: "Designer Cushion",
     sellingPrice: 200,
     actualPrice: 400,
   },
   {
-    image: PenImage,
-    name: "Premium Pen",
-    sellingPrice: 200,
-    actualPrice: 400,
-  },
-  {
-    image: PenImage,
-    name: "Premium Pen",
+    image: WatchBuy5,
+    name: "Customised Mug",
     sellingPrice: 200,
     actualPrice: 400,
   },
@@ -53,7 +53,7 @@ const WatchAndBuy = () => {
               src={datum.image}
               width={200}
               height={200}
-              className="mix-blend-multiply"
+              className="mix-blend-multiply mx-auto object-contain h-[15rem]"
             />
             <div className="bg-neutral-100/80 px-6 py-2 rounded-b-lg">
               <p className="font-medium text-xl">{datum.name}</p>
