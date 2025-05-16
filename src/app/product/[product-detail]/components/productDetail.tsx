a8ed7e096a7979d2f80c889de28ca7a87f512b48
Amit Sah <amitsah04v@gmail.com>
2025-05-16 12:42:18 +0530

    wip

--- src/app/product/[product-detail]/components/productDetail.tsx
+++ src/app/product/[product-detail]/components/productDetail.tsx
@@ -1,60 +1,68 @@
-'use client'
-// import { useState } from "react";
-import ProductCustomization from './productCustomization'
-import FireSvg from '../../../../../public/svg/fireSvg'
-// import { StarSvg } from '../../../../../public/icons'
-import React from 'react'
-import SendWhatsAppMessage from '../../../../helper/helper'
+"use client";
+import SendWhatsAppMessage from "../../../../helper/helper";
+import ProductCustomization from "./productCustomization";
+import FireSvg from "../../../../../public/svg/fireSvg";
+import { StarSvg } from "../../../../../public/icons";
 // import CartDrawer from "./cartDrawer";
+// import { useState } from "react";
+import React from "react";
 
 const ProductDetail = () => {
-    const params = window.location.pathname.split('/').pop().toLowerCase();
-    console.log(params)
+  const params = window.location.pathname.split("/").pop().toLowerCase();
+  console.log(params);
 
-    // const [setIsCartOpen] = useState(false);
-    return (
-        <div className='flex flex-col gap-2 p-3 w-full'>
-            {/* heading */}
-            <p className="font-sub font-semibold text-xl text-balance leading-tight">Product Code <span className='uppercase'>{params}</span></p>
-            {/* stars */}
-            {/* <div className='flex place-items-center'>{[...Array(4)].map((i, index) => (
-                <StarSvg key={index} className='fill-amber-300 stroke-1 stroke-amber-700' />
-            ))}
-                <StarSvg className='stroke-1 stroke-amber-700' />
-                <p className='ml-2 text-neutral-500 text-xs'>reviews (195)</p>
-            </div> */}
-            {/* price tag */}
-            <div className='flex flex-col gap-1 my-0 font-medium'>
-                <div className='flex place-items-center bg-[#cc0c39] px-2 py-1 rounded-sm w-fit font-semibold text-white text-xs 2xl'>Limited time deal</div>
-                <div className='flex flex-col place-items-start py-1'>
-                    <div className='flex gap-2'>
-                        <p className='font-sub font-bold text-lg leading-normal'>Rs. 999</p>
-                        <div className='flex place-items-center bg-orange-400 px-2 py-0.5 rounded-sm font-sub font-semibold text-white text-sm 2xl'>Save 19%</div>
-                    </div>
-                    <p className='text-neutral-400 text-xs line-through'>Rs. 1,238.00</p>
-                </div>
+  // const [setIsCartOpen] = useState(false);
+  return (
+    <div className="flex flex-col gap-2 p-3 w-full">
+      {/* heading */}
+      <p className="font-sub font-semibold text-3xl text-balance leading-tight">
+        Product Code <span className="font-black uppercase">{params}</span>
+      </p>
+      {/* stars */}
+      <div className="flex place-items-center">
+        {[...Array(4)].map((i, index) => (
+          <StarSvg
+            key={index}
+            className="fill-amber-300 stroke-1 stroke-amber-700"
+          />
+        ))}
+        <StarSvg className="stroke-1 stroke-amber-700" />
+        <p className="ml-2 text-neutral-500 text-sm">reviews (195)</p>
+      </div>
+      {/* price tag */}
+      <div className="flex flex-col gap-1 my-0 font-medium">
+        <div className="flex place-items-center bg-[#cc0c39] px-2 py-1 rounded-sm w-fit font-semibold text-white text-xl 2xl">
+          Limited time deal
+        </div>
+        <div className="flex flex-col place-items-start py-1">
+          <div className="flex gap-2">
+            <p className="font-sub font-bold text-2xl leading-normal">Rs. 999</p>
+            <div className="flex place-items-center bg-orange-400 px-2 py-0.5 rounded-sm font-sub font-semibold text-white text-base 2xl">
+              Save 19%
             </div>
-            {/* coupons */}
-            {/* <div className='bg-green-100 px-3 py-0.5 border border-green-500 border-dashed rounded-md w-fit font-semibold text-green-600/90 text-sm'>
+          </div>
+          <p className="text-neutral-400 text-base line-through">Rs. 1,238.00</p>
+        </div>
+      </div>
+      {/* coupons */}
+      {/* <div className='bg-green-100 px-3 py-0.5 border border-green-500 border-dashed rounded-md w-fit font-semibold text-green-600/90 text-sm'>
                 ⭐ 12% OFF | Coupon Code - FESTWEEK12
             </div> */}
-            {/* last hour orders */}
-            <div className='flex place-items-center gap-1 font-medium text-neutral-800 text-xs'><span className='animate-pulse'><FireSvg /></span> 18 orders in last 5 hours </div>
 
-            {/* customization */}
-            <ProductCustomization />
+      {/* customization */}
+      <ProductCustomization />
 
-            {/*Add to cart */}
-            <button
-                className="flex justify-center bg-black/70 active:bg-black/30 mt-2 px-12 py-2 border rounded-sm w-80 font-semibold text-white text-sm hover:scale-105 active:scale-95 transition-all cursor-pointer"
-                onClick={() => SendWhatsAppMessage(params,params,1)}
-            >
-                Buy
-            </button>
-            {/* Cart Drawer */}
-            {/* <CartDrawer isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} /> */}
-        </div>
-    )
-}
+      {/*Add to cart */}
+      <button
+        className="flex justify-center bg-black/70 active:bg-black/30 mt-2 px-12 py-2 border rounded-sm w-80 font-semibold text-white text-xl hover:scale-105 active:scale-95 transition-all cursor-pointer"
+        onClick={() => SendWhatsAppMessage(params, params, 1)}
+      >
+        Buy
+      </button>
+      {/* Cart Drawer */}
+      {/* <CartDrawer isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} /> */}
+    </div>
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
--- src/app/product/[product-detail]/page.tsx
+++ src/app/product/[product-detail]/page.tsx
@@ -1,59 +1,75 @@
-'use client'
-import { Category1, Category2, Category3, Category4 } from '@public/images'
-import ProductDetail from './components/productDetail'
-import React, { useState } from 'react'
-import { StarSvg } from '@public/icons'
-import Image from 'next/image'
-import ProductFeatures from './components/productFeatures'
-import Footer from '../../../components/footer'
-// import DynamicCarousel from '../../../components/homePage/carousel'
-// import { LISTING_DATA } from '../../all-products/page'
+"use client";
 // import ProductCart, { IProductData } from '../../../components/homePage/productCart'
-import Reviews from '../../../components/homePage/reviews'
+import { Category1, Category2, Category3, Category4 } from "@public/images";
+// import DynamicCarousel from '../../../components/homePage/carousel'
+import ProductFeatures from "./components/productFeatures";
+import Reviews from "../../../components/homePage/reviews";
+// import { LISTING_DATA } from '../../all-products/page'
+import ProductDetail from "./components/productDetail";
+import Footer from "../../../components/footer";
+import React, { useState } from "react";
+// import { StarSvg } from "@public/icons";
+import Image from "next/image";
 
 const Page = () => {
-  const [mainImage, setMainImage] = useState(Category1)
-  const handleImageSelect = (image: string) => {
-    setMainImage(image);
-  };
+  const [mainImage, setMainImage] = useState(Category1);
+  // const handleImageSelect = (image: string) => {
+  //   setMainImage(image);
+  // };
 
-  const IMAGEDATA = [Category1, Category2, Category3, Category4];
+  // const IMAGEDATA = [Category1, Category2, Category3, Category4];
 
   return (
     <>
-      <div className='mx-auto pt-2 w-10/12'>
-        <div className='gap-4 grid grid-cols-2'>
+      <div className="mx-auto pt-2 w-10/12">
+        <div className="gap-4 grid grid-cols-2">
           {/* product images */}
-          <div className='flex bg-neutral-50 shadow border border-neutral-100 rounded-sm w-full'>
+          <div className="flex bg-neutral-50 shadow border border-neutral-100 rounded-sm w-full">
             {/* Select image */}
-            <div className='flex flex-col gap-2 my-2 w-3/12 max-h-[400px]'>
+            {/* <div className="flex flex-col gap-2 my-2 w-3/12 max-h-[400px]">
               {IMAGEDATA.map((image, index) => (
-                <Image key={index} src={image} alt={`image ${index + 1}`} height={200} width={200}
-                  className={`px-2 w-full max-h-[120px] max-w-[110px] h-full rounded-sm object-cover aspect-square hover:scale-110 transition-all cursor-pointer ${mainImage === image ? 'border border-neutral-300 p-2 rounded-xs ' : 'opacity-60'}`}
+                <Image
+                  key={index}
+                  src={image}
+                  alt={`image ${index + 1}`}
+                  height={200}
+                  width={200}
+                  className={`px-2 w-full max-h-[120px] max-w-[110px] h-full rounded-sm object-cover aspect-square hover:scale-110 transition-all cursor-pointer ${
+                    mainImage === image
+                      ? "border border-neutral-300 p-2 rounded-xs "
+                      : "opacity-60"
+                  }`}
                   onClick={() => handleImageSelect(image)}
                 />
               ))}
-            </div>
+            </div> */}
 
             {/* Main image */}
-            <div className='relative w-full h-full'>
-              <Image src={mainImage} alt='main image' height={400} width={400} className='py-2 pr-2 w-full max-h-[480px] object-cover' />
-              <div className='top-1 right-0 absolute flex gap-1 bg-red-500 shadow-md px-4 py-1 border border-white rounded-full font-sub font-semibold text-white text-sm capitalize'>
-                <StarSvg className='!stroke-white' />
+            <div className="relative w-full h-full">
+              <Image
+                src={mainImage}
+                alt="main image"
+                height={400}
+                width={400}
+                className="py-2 pr-2 w-full max-h-[480px] object-cover"
+              />
+              {/* <div className="top-1 right-0 absolute flex gap-1 bg-red-500 shadow-md px-4 py-1 border border-white rounded-full font-sub font-semibold text-white text-sm capitalize">
+                <StarSvg className="!stroke-white" />
                 <p>best seller</p>
-              </div>
+              </div> */}
             </div>
           </div>
 
           {/* product details */}
           <ProductDetail />
-
         </div>
         {/* Product Features */}
-        <ProductFeatures />
+        {/* <ProductFeatures /> */}
         {/* carousel */}
         <div>
-          <p className='py-4 pt-16 font-main font-semibold text-center'>Corporate Gift Collection</p>
+          <p className="py-4 pt-16 font-main font-semibold text-center">
+            Corporate Gift Collection
+          </p>
           {/* <DynamicCarousel
             data={LISTING_DATA}
             breakpoints={5}
@@ -62,11 +78,11 @@ const Page = () => {
             )}
           /> */}
         </div>
-          <Reviews />
+        {/* <Reviews /> */}
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
